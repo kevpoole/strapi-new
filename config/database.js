a73dfdc11bae5587222672ee35203704094a138e
Kevin Poole <kevitron@hotmail.com>
2022-01-31 02:18:39 -0500

    middleware

--- config/database.js
+++ config/database.js
@@ -9,13 +9,8 @@ module.exports = ({ env }) => ({
         "..",
         env("DATABASE_FILENAME", ".tmp/data.db")
       ),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      
     },
     useNullAsDefault: true,
-    options: {
-      ssl: env.bool('DATABASE_SSL', false),
-    },
   },
 });
